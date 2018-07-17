ba0b60e1b2e3d72c810d5ca5ce0120318c41798f
huypq6 <huypq6@outlook.com.vn>
2018-07-17 23:13:11 +0700

    Update installessential.sh

--- installation/installessential.sh
+++ installation/installessential.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+#Install tweak tools for gnome 3
+sudo apt install \
+  gnome-tweak-tool \
+  
+
 #Install essential development packages
-```
-$ sudo apt-get install gcc-multilib unzip build-essential chrpath diffstat gawk git-core libncurses5-dev pkg-config subversion texi2html texinfo libsdl1.2-dev xterm
-```
+sudo apt install \
+  gcc-multilib \
+  unzip \
+  build-essential \
+  chrpath \
+  diffstat \
+  gawk \
+  git-core \
+  libncurses5-dev \
+  pkg-config \
+  subversion \
+  texi2html \
+  texinfo \
+  libsdl1.2-dev \
+  xterm
+
+#Install texlive and pandoc
+sudo apt install \
+  texlive-full \
+  pandoc 
