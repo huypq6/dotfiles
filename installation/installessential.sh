3d3acc81d1ae15899a6c01b786f72edde70d0928
huypq6 <huypq6@outlook.com.vn>
2018-07-17 22:42:15 +0700

    Create installessential.sh

---
+++ installation/installessential.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Install essential development packages
+```
+$ sudo apt-get install gcc-multilib unzip build-essential chrpath diffstat gawk git-core libncurses5-dev pkg-config subversion texi2html texinfo libsdl1.2-dev xterm
+```
