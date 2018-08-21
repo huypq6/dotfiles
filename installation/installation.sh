d90ee26b3d4e5a389dcf1cf1f3b3c0a17292a9e1
Huy Pham <huypq6@outlook.com.vn>
2018-08-21 21:05:58 +0700

    installation package for archlinux

---
+++ installation/installation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+progsfile=./packages.csv
+
+maininstall() { # Installs all needed programs from main repo.
+	echo "-> Installing ""$1"" from Main Repo."
+	sudo pacman --noconfirm --needed -S "$1" #&>/dev/null
+	}
+
+aurinstall() { \
+	echo "-> Installing " "$1"" from AUR."
+	yaourt --noconfirm --needed -S "$1" #&>/dev/null 
+}
+
+installationloop() { \
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" > /tmp/progs.csv
+	total=$(wc -l < /tmp/progs.csv)
+	aurinstalled=$(pacman -Qm | awk '{print $1}')
+	while IFS=, read -r Package Description Group repo; do
+	n=$((n+1))
+	case $repo in
+	"") maininstall "$Package" ;;
+	"A") aurinstall "$Package" ;;
+	"G") gitmakeinstall "$Package" ;;
+	"*") echo "Unknow Repo.";;
+	esac
+done < /tmp/progs.csv ;}
+
+echo "Start Installation loop..."
+installationloop
