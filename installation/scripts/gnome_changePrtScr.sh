092a902f3f0c419c635b7493a74ba6e732991517
Huy Pham <huypq6@outlook.com.vn>
2019-08-20 12:17:21 +0700

    added change print screen shortcut to flameshot

---
+++ installation/scripts/gnome_changePrtScr.sh
@@ -0,0 +1,18 @@
+
+# Change Print Screen shortcut to using flameshot
+
+# Release binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot ''
+
+# Set new custom binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+
+# Set name
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
+
+# Set Command
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command '/usr/bin/flameshot gui'
+
+# Set Binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Print'
+
